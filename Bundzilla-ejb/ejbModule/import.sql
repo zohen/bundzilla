ec892a6ca4cd7a90758abb9b219f8ed1e408ed3f
mihai.poplacenel <mihai.poplacenel@612cf738-2337-0410-922a-a9ff42f9b2a7>
2008-01-16 06:58:31 +0000

    Got the TestNG tests to work from command line (ANT).

--- Bundzilla-ejb/ejbModule/import.sql
+++ Bundzilla-ejb/ejbModule/import.sql
@@ -1,5 +1,5 @@
-insert into Role (id, name, version) values (1, 'admin', 1);
-insert into Role (id, name, version) values (2, 'user', 1);
-insert into User (id, name, password, email, version) values (1, "admin", "admin", "admin@bundzilla.com", 1);
-insert into UserRole (id, user_id, role_id, version) values (1, 1, 1, 1);
-insert into UserRole (id, user_id, role_id, version) values (2, 1, 2, 1);
+insert into APP_Role (id, name, version) values (1, 'admin', 1);
+insert into APP_Role (id, name, version) values (2, 'user', 1);
+insert into APP_User (id, name, password, email, version) values (1, 'admin', 'admin', 'admin@bundzilla.com', 1);
+insert into APP_UserRole (id, user_id, role_id, version) values (1, 1, 1, 1);
+insert into APP_UserRole (id, user_id, role_id, version) values (2, 1, 2, 1);
